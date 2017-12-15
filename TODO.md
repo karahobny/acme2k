8a1c6d6432ace97f212ea1e8bb74c4b2334e83b6
karahobny <33952679+karahobny@users.noreply.github.com>
2017-12-15 17:26:06 +0200

    x11.c with maybe better font-rendering found via @frederich's gist

---
+++ TODO.md
@@ -0,0 +1,4 @@
+### todo
++ .diff -file for easier application (modified files so far: acme.c, mkfile, config.h)
++ fontsrv-modification (LCDFILTER? hardcoded dpi? font-height?: *see $PLAN9/src/cmd/fontsrv/x11.c*)
++ add more to do (contribute by adding an issue)
---
+++ fontsrv/a.h
@@ -0,0 +1,29 @@
+typedef struct XFont XFont;
+XFont *xfont;
+int nxfont;
+
+struct XFont
+{
+	char *name;
+	int loaded;
+	uchar range[256];	// range[i] == whether to have subfont i<<8 to (i+1)<<8.
+	int nrange;
+	int unit;
+	double height;
+	double originy;
+
+	// fontconfig workarround, as FC_FULLNAME does not work for matching fonts.
+	char *fontfile;
+	int  index;
+};
+
+void	loadfonts(void);
+void	load(XFont*);
+Memsubfont*	mksubfont(char*, int, int, int, int);
+
+extern XFont *xfont;
+extern int nxfont;
+void *emalloc9p(ulong);
+extern Memsubfont *defont;
+
+void	drawpjw(Memimage*, Fontchar*, int, int, int, int);
