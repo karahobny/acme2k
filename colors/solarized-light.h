e21a9b24e7883a5e330b4b13005e2a72dcce9418
Neale Pickett <neale@lanl.gov>
2020-04-28 09:39:45 -0600

    Add solarized light colors

---
+++ colors/solarized-light.h
@@ -0,0 +1,39 @@
+/* Solarized pallette */
+#define SOL_BASE03 0x002b36ff
+#define SOL_BASE02 0x073642ff
+#define SOL_BASE01 0x586e75ff
+#define SOL_BASE00 0x657b83ff
+#define SOL_BASE0 0x839496ff
+#define SOL_BASE1 0x93a1a1ff
+#define SOL_BASE2 0xeee8d5ff
+#define SOL_BASE3 0xfdf6e3ff
+#define SOL_YELLOW 0xb58900ff
+#define SOL_ORANGE 0xcb4b16ff
+#define SOL_RED 0xdc322fff
+#define SOL_MAGENTA 0xd33682ff
+#define SOL_VIOLET 0x6c71c4ff
+#define SOL_BLUE 0x268bd2ff
+#define SOL_CYAN 0x2aa198ff
+#define SOL_GREEN 0x859900ff
+
+/* Tag area */
+#define C_TAGBG SOL_BASE2
+#define C_TAGFG SOL_BASE00
+#define C_TAGHLBG SOL_BASE3
+#define C_TAGHLFG SOL_BLUE
+
+/* Text area */
+#define C_TXTBG SOL_BASE3
+#define C_TXTFG SOL_BASE00
+#define C_TXTHLBG SOL_BASE2
+#define C_TXTHLFG C_TXTFG
+
+/* That little rectangle at the top left, and the scrollbar */
+#define C_WINBUTTON SOL_BASE0
+#define C_COLBUTTON SOL_BASE0
+#define C_TMPBUTTON SOL_MAGENTA
+#define C_SCROLLBG SOL_BASE0
+
+/* Highlight for dragging with mouse buttons 2 and 3 */
+#define C_BUTTON2HL SOL_CYAN
+#define C_BUTTON3HL SOL_VIOLET
